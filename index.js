e22de1a7ebafdede2840bbd912bcc35089ce938d
shajib1927 <shajibshahrear@gmail.com>
2023-08-26 06:57:36 +0300

    news updated

--- index.js
+++ index.js
@@ -4,15 +4,35 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const categories = require('./categories.json')
+const news = require('./news.json')
 
 app.use(cors())
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Working Properly')
 })
-
 app.get('/categories', (req, res) => {
     res.send(categories)
+})
+
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+app.get('/news/:id', (req, res) => {
+  const id = req.params.id;  
+  console.log(id);
+  const selectedNews = news.find( n => n._id);
+  res.send(selectedNews)
+})
+
+app.get('/categories/:id', (req, res) => {
+  const id = req.params.id;  
+  console.log(id);
+  const categoryNews = news.filter( n => n.category_id == id);
+  if (id == 0) {
+    res.send(news)
+  }
+  else res.send(categoryNews)
 })
 
 app.listen(port, () => {
